897384f79374741d48916217612b712864c868de
seohg <seohg@seohgui-MacBook-Pro.local>
2021-05-13 22:07:59 +0900

    insert

--- app/schema/test.sql
+++ app/schema/test.sql
@@ -2,8 +2,56 @@ CREATE DATABASE testDB default CHARACTER SET UTF8;
  
 use testDB;
  
-CREATE TABLE testTable(
-    idx      INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    test     VARCHAR(256) NOT NULL
-) CHARSET=utf8;
+
+CREATE TABLE category(
+   CID INTEGER PRIMARY KEY AUTO_INCREMENT,
+   category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE institution(
+   institution_name VARCHAR(200) PRIMARY KEY,
+   institution_url VARCHAR(600),
+   major_cid INTEGER,
+   FOREIGN KEY (major_cid) REFERENCES category(CID)
+);
+
+CREATE TABLE writer(
+   WID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   writer_name VARCHAR(200),
+   UNIQUE KEY writername (writer_name)
+);
+
+CREATE TABLE research(
+ RID INTEGER PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(200) NOT NULL,
+ research_url VARCHAR(600) NOT NULL,
+ institution_name VARCHAR(200),
+ CID INTEGER,
+ WID INTEGER,
+ tot_word_cnt INTEGER NOT NULL,
+ FOREIGN KEY (institution_name) REFERENCES institution(institution_name),
+ FOREIGN KEY (CID) REFERENCES category(CID),
+ FOREIGN KEY (WID) REFERENCES writer(WID)
+);
+
+CREATE TABLE pub_date(
+   RID INTEGER,
+   year INTEGER NOT NULL,
+   month INTEGER NULL,
+   date INTEGER NULL,
+   FOREIGN KEY (RID) REFERENCES research(RID)
+);
+
+CREATE TABLE keyword(
+   word VARCHAR(100) PRIMARY KEY,
+   RID INTEGER,
+   freq INTEGER NOT NULL,
+   FOREIGN KEY (RID) REFERENCES research(RID)
+);
+
+CREATE TABLE content_date(
+   RID INTEGER,
+   decade INTEGER,
+   FOREIGN KEY (RID) REFERENCES research(RID)
+);
 
--- sample_client_dist.py
+++ sample_client_dist.py
@@ -3,58 +3,134 @@ import urllib.parse as p
 import json
 import pandas as pd
 import ssl
+import re
 
 from app.module import dbModule
 
-url = "https://kubic.handong.edu:15000/retrieve_all?"
-serviceKey = "QyEqZtZ1vC-MNfq_NgsBEQ"
-numOfCnt = 200
-page =2
-
-option = "serviceKey="+serviceKey
-request = ""
-request = "&numOfCnt="+p.quote(str(numOfCnt))+"&page="+p.quote(str(page))
-url_full = url + option + request
+for page in range(1,49):
+  '''
+  connected page
+  '''
+  url = "https://kubic.handong.edu:15000/retrieve_all?"
+  serviceKey = "QyEqZtZ1vC-MNfq_NgsBEQ"
+  numOfCnt = 200
 
 
-print("url>"+url_full+"numOfCnt="+"10")
-context = ssl._create_unverified_context()
-response = r.urlopen(url_full,context=context).read().decode('utf-8')
-#print(response)
+  option = "serviceKey="+serviceKey
+  request = ""
+  request = "&numOfCnt="+p.quote(str(numOfCnt))+"&page="+p.quote(str(page))
+  url_full = url + option + request
 
-jsonArray = json.loads(response)
 
-if jsonArray.get("header").get("resultCode") != 200:
-    #print("Error!!!")
+  print("url>"+url_full)
+  context = ssl._create_unverified_context()
+  response = r.urlopen(url_full,context=context).read().decode('utf-8')
+  #print(response)
+
+  jsonArray = json.loads(response)
+
+  if jsonArray.get("header").get("resultCode") != 200:
+    print("Error!!!")
     print(jsonArray.get("header"))
     quit()
 
-items =jsonArray.get("body").get("contents")
-#print("items>", items)
+  items =jsonArray.get("body").get("contents")
+  #print("items>", items)
 
-df = pd.DataFrame(columns=['title', 'body', 'writer', 'date', 'category', 'institution', 'file'])
-for item in items:
+  df = pd.DataFrame(columns=['title', 'body', 'writer', 'date', 'institution', 'institutionURL', 'fileURL', 'fileName','fileContent'])
+  for item in items:
     df = df.append(item, ignore_index=True)
-  #  print(df)
+  
+   
+      
 
+  db_class = dbModule.Database()
 
-'''
- 아래 코드는 임의로 간단한 테이블 만들어서 db에 데이터가 들어가는지 확인하려고 간단하게 틀만 만들어둔 부분입니다.
- 데이터베이스를 설계해서 만든 후에 page로 for문 돌리고 sql 수정한 후에 넣으면 될 것 같아요!
- dbModule.py에서 DB 정보 수정한 후에 사용하세요
- 그리고 테이블 만들때 한글 설정 안하면 오류나요~ 꼭 설정해주세용ㅎㅎ
-'''
+  for i, item in df.iterrows():
 
-db_class = dbModule.Database()
-for i, item in df.iterrows():
+    if item.title is not None:
+      item.title = item.title.strip()
+      item.title = item.title.replace("\'","\\\'")
+      item.title = item.title.replace("%","%%")
 
-    #print(item.title)
-    sql= "INSERT INTO testDB.title(title) VALUES('%s')"%item.title
-    # testDB = db이름 , title=table이름 ,(title) = attribute이름
+    if item.fileURL is not None:
+      item.fileURL = item.fileURL.strip()
+      item.fileURL = item.fileURL.replace("\'","\\\'")
+      item.fileURL = item.fileURL.replace("%","%%")
+
+    if item.writer is not None:
+      item.writer = item.writer.strip()
+      item.writer = item.writer.replace("\'","\\\'")
+      item.writer = item.writer.replace("%","%%")
+    # institution
+    print(i)
+    print(item.institution)
+    print("=====")
+    print(item.fileURL)
+
+    sql1= "INSERT INTO testDB.institution(institution_name,institution_url) VALUES('%s','%s') ON DUPLICATE KEY UPDATE institution_name='%s'"%(item.institution, item.fileURL,item.institution)
+    db_class.execute(sql1)
+
+    # writer
+    sql2= "INSERT INTO testDB.writer VALUES (NULL,'%s') on duplicate key update writer_name=writer_name"%(item.writer)
+    db_class.execute(sql2)
+
+    print("here")
+    print(item.title)
+    print(item.fileURL)
+    print(item.institution)
+    print("there")
     
-    db_class.execute(sql)
+    
+    
+    #research
+    sql3= "INSERT INTO testDB.research(title,research_url,institution_name,tot_word_cnt) VALUES('%s','%s','%s','%s')"%(item.title, item.fileURL, item.institution, 0)
+    print()
+    # testDB = db이름 , title=table이름 ,(title) = attribute이름
+    db_class.execute(sql3)
+    rid = db_class.cursor.lastrowid
+    db_class.commit()
+    
+
+    wid = db_class.executeOne("select WID from testDB.writer where writer_name='%s'"%item.writer)
+
+    print(wid)
+    print("!!!")
+    db_class.commit()
+
+    sql2_1= "update testDB.research set WID=('%s') where testDB.research.RID='%d'"%(wid['WID'],rid)
+    db_class.execute(sql2_1)
+    db_class.commit()
+
+    if item.date is not None:
+      item.date = item.date.replace("\n","")
+      item.date = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', item.date)
+    
+      if len(item.date)>=8:
+        year = int(item.date[0:4])
+        month = int(item.date[4:6])
+        date = int(item.date[6:8])
+        sql4= "INSERT INTO testDB.pub_date(RID,year,month,date) VALUES('%s','%s','%s','%s')"%(rid,year, month, date)
+    
+
+      elif len(item.date)>=6:
+        year = int(item.date[0:4])
+        month = int(item.date[4:6])
+        sql4= "INSERT INTO testDB.pub_date(RID,year,month) VALUES('%s','%s','%s')"%(rid,year, month)
+
+      elif len(item.date)>=4:
+        year = int(item.date[0:4])
+        sql4= "INSERT INTO testDB.pub_date(RID,year) VALUES('%s','%s')"%(rid,year)
+      
+      else:
+        sql4= "INSERT INTO testDB.pub_date(RID,year) VALUES('%s','%s')"%(rid,item.date)
+      db_class.execute(sql4)
+
+    db_class.commit()
+
     #print(item)
 
-    #break
     
-db_class.commit()
+  
+
+
