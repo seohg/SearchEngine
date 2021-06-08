e5ce9b5f9f1de245cd61bb16b990292cafe9ae04
YuSangGon0505 <83908550+YuSangGon0505@users.noreply.github.com>
2021-06-09 00:45:45 +0900

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
-from flask import Flask
- 
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 from app.main.index import main as main
 from app.test.test import test as test
 
 app.register_blueprint(test)
 app.register_blueprint(main)
+
+@app.route('/')
+def website():
+    return render_template('website.html')
+
+@app.route('/search',methods=['POST','GET'])
+def search():
+    title = request.form
+    return render_template("search.html", value = title)
+
+@app.route('/info',methods=['POST','GET'])
+def info():
+    return render_template("info.html")
