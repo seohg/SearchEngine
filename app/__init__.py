6e38bc74777c0fe7428d11ea1101bb94a5e57c77
seohg <63279356+seohg-hgu@users.noreply.github.com>
2021-06-17 22:38:07 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,22 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 from app.main.index import main as main
 from app.test.test import test as test
+import BM25
 
 app.register_blueprint(test)
 app.register_blueprint(main)
 
 @app.route('/')
-def first():
-    return render_template('first.html')
-
-@app.route('/website')
 def website():
     return render_template('website.html')
 
 @app.route('/search',methods=['POST','GET'])
 def search():
     title = request.form
+    BM25_return = BM25.findDocumentsForQuery(title)
+
     return render_template("search.html", value = title)
 
 @app.route('/info',methods=['POST','GET'])
 def info():
-    ti = request.form
-    return render_template("info.html", val = ti)
+    return render_template("info.html")
